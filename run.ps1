8d176444bea6a0c55b8c63386065a9dc3fcc50e1
WangWindow <1598593280@qq.com>
2024-12-26 23:22:05 +0800

    Upload: run scripts.

---
+++ run.ps1
@@ -0,0 +1,1 @@
+dotnet run --project "./src/IoTGame.Practice.Client/IoTGame.Practice.Client.csproj"
---
+++ test.ps1
@@ -0,0 +1,1 @@
+dotnet watch --project "./src/IoTGame.Practice.Client/IoTGame.Practice.Client.csproj"
